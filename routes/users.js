99a85ffd3a139a94f3c6e44cd9dda3ce88e587fb
RobinRoqueza <robroqueza@hotmail.com>
2018-10-14 22:22:33 +1100

    view profile for doctors/students

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const doctors = require('../data/staff.json');
-const students = require('../data/students.json');
-const appointments = require('../data/appointments.json');
+const doctors = require("../data/doctor.json");
+const students = require("../data/students.json");
+const appointments = require("../data/appointments.json");
 
 //Home Page
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/appointment', (req, res, next) => {
-  res.render('appointment');
+router.get("/appointment", (req, res, next) => {
+  res.render("appointment");
 });
 
-router.get('/profile', (req, res, next) => {
-  const app = [];   
+router.get("/profile", (req, res, next) => {
+  const app = [];
   const listOfappointments = students[1].appointments;
 
   if (listOfappointments.length > 0) {
@@ -28,19 +28,26 @@ router.get('/profile', (req, res, next) => {
     }
   }
 
-
-
   console.log(app);
-  res.render('profile', {
+  res.render("profile", {
     app,
-    doctors
+    doctors,
+    students
   });
 });
 
+router.post("/profile", (req, res) => {
+  console.log(req.body.new_firstname);
+  res.render("profile", {
+    app,
+    doctors,
+    students
+  });
+});
 
-router.post('/appointment', (req, res, next) => {
+router.post("/appointment", (req, res, next) => {
   // res.json(req.body);
-  res.render('success.ejs');
+  res.render("success.ejs");
 });
 
 module.exports = router;
