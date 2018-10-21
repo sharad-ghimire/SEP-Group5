edb67d0a3f818c8068dc90859d05610be326225d
Vincent Nguyen <hoangyzn@gmail.com>
2018-10-22 02:42:12 +1100

    In progress

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@
 var chai = require("chai");
 var mongoose = require("mongoose");
 var User = require("../models/user");
+var Student = require("../models/student");
 
 var superagent = require("superagent");
 var agent = superagent.agent();
@@ -25,21 +26,41 @@ var loginDetails = {
 var registerDetails = {
   name: "vincent",
   stdId: "3290",
+  email: "3290@gmail.com",
   password: "vivid",
   password2: "vivid",
-  isDoctor: false
+  isDoctor: "2"
 };
+var appointmentDetails = {
+  medicalType : "hiv",
+  date: "2018-10-08",
+  timeneeded: "10:30 AM",
+  doctor: "Ted Mosby",
+  stdId: "3290"
+}
 
-describe("Create Account and Log in", function() {
+describe("Create Account and Testing", function() {
   this.timeout(0);
+  //delete account before testing
   before(function(done) {
-    User.remove({ name: "vincent" }, function(err, user) {
+    User.deleteOne({ name: "vincent" }, function(err, user) {
       if (err) return console.error(err);
     });
     done();
   });
-
+  //conncetion testing
+  describe("Connection test", function(){
+    it("it should display homepage", function(done){
+      chai.request("http://localhost:3000")
+      .get("/")
+      .end(function(err, res){
+        expect(res).to.have.status(200);
+        done();
+      })
+    })
+  });
   describe("/POST Register and /POST Login", function() {
+    //Test register function
     it("it should Register", function(done) {
       chai
         .request("http://localhost:3000")
@@ -51,19 +72,39 @@ describe("Create Account and Log in", function() {
           done();
         });
     });
+    //Test if the user is saved
+    it("database should save user", function(done){
+      User.findOne({stdId:"3290"}, function(err, result){
+        expect(result.stdId).to.not.be.undefined;
+        expect(result.password).to.not.be.undefined;
+        expect(result.name).to.not.be.undefined;
+        Student.findOne({stdId:"3290"}, function(err, result2){
+          expect(result2.email).not.to.be.undefined;
+        done();
+      })
+    })
+    });
+    //Test the log in function
     it("it should Login", function(done) {
       agent
         .post("http://localhost:3000/login")
-        .set("Token", "text/plain")
-        .set("content-Type", "application/x-www-form-urlencoded")
-        .type("form")
-        .send({ stdId: "3290" })
-        .send({ password: "vivid" })
+        .send(loginDetails)
         .end((err, res) => {
           expect(res).to.redirectTo("http://localhost:3000/users/profile");
           res.should.have.status(200);
           done();
-        });
+      })
     });
+    //Test appointment function
+    it("it should make an appointment", function(done){
+      chai
+        .request("http://localhost:3000/")
+        .post("users/appointment")
+        .send(appointmentDetails)
+        .end((err,res) =>{
+          expect(res).to.have.status(200);
+          done();
+        })
+    })
   });
 });
